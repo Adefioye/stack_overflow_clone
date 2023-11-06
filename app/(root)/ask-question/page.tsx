ddbf5642f8ea241e68891533ee4bc960791f45e0
Adefioye <abdulhakeemadefioye@gmail.com>
2023-11-06 05:49:55 -0600

    Add forms for ask-a-question-page

--- app/(root)/ask-question/page.tsx
+++ app/(root)/ask-question/page.tsx
@@ -1,7 +1,17 @@
-import React from "react";
+import Question from '@/components/forms/Question'
+import React from 'react'
 
-const AskQuestion = () => {
-  return <div>Ask Question</div>;
-};
+const Page = async () => {
 
-export default AskQuestion;
+  return (
+    <div>
+      <h1 className="h1-bold text-dark100_light900">Ask a question</h1>
+
+      <div className="mt-9">
+        <Question mongoUserId={""}/>
+      </div>
+    </div>
+  )
+}
+
+export default Page
